867a6a10337ed5c164af892fc8816a97d706be5e
Jebisan Hari Nadarajah <jena@hesehus.dk>
2019-10-02 14:55:46 +0200

    more details to bill

--- src/ComponentToPrint.js
+++ src/ComponentToPrint.js
@@ -1,8 +1,13 @@
 import React, { useRef } from 'react';
 import ReactToPrint from 'react-to-print';
-import logo from './logo.png'
+import logo from './logo.png';
+import stregkode from './stregkode.jpg'
 import './App.css';
-import axios from 'axios'
+import axios from 'axios';
+import data from './data.js'
+var JsBarcode = require('jsbarcode');
+
+
 
 
 class ComponentToPrint extends React.Component {
@@ -11,59 +16,134 @@ class ComponentToPrint extends React.Component {
     this.state = {
       products: [],
       totalPrice: 0,
-      moms: 0
     }
   }
 
   componentDidMount() {
     axios.get('https://my-json-server.typicode.com/Jebisan/db/data').then((response) => {
       this.setState({ products: response.data })
-    }).then (()=>
-    this.state.products.forEach(product => {
-      this.setState((prevState)=>{
-        return {totalPrice:prevState.totalPrice+=product.PRICE}})
-    })
+    }).then(() =>
+      this.state.products.forEach(product => {
+        this.setState((prevState) => {
+          return { totalPrice: prevState.totalPrice += product.PRICE }
+        })
+      })
     );
+
+    JsBarcode("#barcode", "14192000057464", {
+      format: "code39",
+      lineColor: "#000",
+      width: 1.5,
+      height: 35,
+      displayValue: true
+    });
   }
 
   render() {
     return (
       <div className="App">
         <img src={logo} className="logo" alt="logo" />
+        <div className="headerinfo">
+          <p>{data.header}</p>
+          <p>{data.address}</p>
+          <p>Tlf. {data.tlf}</p>
+          <p>CVR nr.: {data.cvr}</p>
+          <p>Butiksnr.: {data.butiksnr}</p>
+        </div>
+
+        <div className='products'>
+          <table >
+            <tbody>
+              {this.state.products.map((product, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="product" >{product.PRODUCT}</td>
+                    <td className="price" >{product.PRICE} DKK</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+
+
         <table>
           <tbody>
-          <div className="productsContainer" >
-            {this.state.products.map((product, index) => {
-              return (
-                <tr key= {index}>
-                  <td className="product" >{product.PRODUCT}</td>
-                  <td className="price" >{product.PRICE} DKK</td>
-                </tr>
-              )})}
-          </div>
-              <div className="totalContainer">
-
-
-              <tr>
-                  <td className="totalText"></td>
-                </tr><tr>
-                  <td className="totalText"></td>
-                </tr><tr>
-                  <td className="totalText"></td>
-                </tr>
-
-
-                <tr>
-                  <td className="totalText">AT BETALE</td>
-                  <td className="totalPrice" >{(this.state.totalPrice).toFixed(2)} DKK</td>
-                </tr>
-                <tr>
-                  <td className="momsText">HERAF UDGØR MOMS</td>
-                  <td className="momsPrice" >{(this.state.totalPrice/5).toFixed(2)} DKK</td>
-                </tr>
-                </div>
+            <tr>
+              <td className="totalText"></td>
+            </tr>
+            <tr>
+              <td className="totalText"></td>
+            </tr>
+            <tr>
+              <td className="totalText"></td>
+            </tr>
+            <tr>
+              <td className="totalText">AT BETALE</td>
+              <td className="totalPrice" >{(this.state.totalPrice).toFixed(2)} DKK</td>
+            </tr>
+            <tr>
+              <td className="momsText">HERAF UDGØR MOMS</td>
+              <td className="momsPrice" >{(this.state.totalPrice / 5).toFixed(2)} DKK</td>
+            </tr>
           </tbody>
         </table>
+
+        <table className="footerinfo">
+          <tbody>
+            <tr className="flex-container">
+              <td>Du blev betjent af: {data.betjening}</td>
+              <td className="right" >Kasse: {data.kasse}</td>
+            </tr>
+            <tr className="flex-container">
+              <td>Dato: {data.dato}</td>
+              <td className="right" >Tid: {data.tid} </td>
+            </tr>
+            <tr>
+              <td className="">Bon nr.: {data.bon}</td>
+            </tr>
+          </tbody>
+        </table>
+        <p></p>
+        <div className="club">
+          <p>****************************************************</p>
+          <p>{data.club}</p>
+          <p>****************************************************</p>
+        </div>
+
+      <div className="bytteinfo" >
+        <p>{data.bytteservice[0]}</p>
+        <p>{data.bytteservice[1]}</p>
+      </div>
+
+        <div className="openinghourscontainer">
+          <p className='day'>Åbningstider:</p>
+          <div className="day">
+          <p>Mandag:</p> <p className="openingtime">{data.openinghours.mandag}</p>
+          </div>
+          <div className="day">
+          <p>Tirsdag  :</p> <p className="openingtime">{data.openinghours.tirsdag}</p>
+          </div>
+          <div className="day">
+          <p>Onsdag:</p> <p className="openingtime">{data.openinghours.onsdag}</p>
+          </div>
+          <div className="day">
+          <p>Torsdag:</p> <p className="openingtime">{data.openinghours.torsdag}</p>
+          </div>
+          <div className="day">
+          <p>Fredag  :</p> <p className="openingtime">{data.openinghours.fredag}</p>
+          </div>
+          <div className="day">
+          <p>Lørdag  :</p> <p className="openingtime">{data.openinghours.lørdag}</p>
+          </div>
+          <div className="day">
+          <p>Søndag:</p> <p className="openingtime">{data.openinghours.søndag}</p>
+          </div>
+        </div>
+        <svg id="barcode"></svg>
+
+
+
       </div>
     );
   }
---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data = {
+    header: 'Matas Rosengårdscentret',
+    address: 'Ørbækvej 75, 5220 Odense',
+    tlf: '66 15 96 68',
+    cvr: '27528430',
+    butiksnr: '10040',
+    betjening: 'Lene B.',
+    kasse:'5',
+    dato: '06/06/2019',
+    tid: '15:12:31',
+    bon: '00035091',
+    openinghours: {
+        mandag: '10.00 - 19.00',
+        tirsdag: '10.00 - 19.00',
+        onsdag: '10.00 - 19.00',
+        torsdag: '10.00 - 19.00',
+        fredag: '10.00 - 19.00',
+        lørdag: '10.00 - 17.00',
+        søndag: '10.00 - 17.00',
+          
+    },
+    bytteservice: ['Landsdækkende bytteservice i Matas.','Husk byttemærke eller bon. Se mere på matas.dk'],
+    club: "BESKED FRA CLUB MATAS"
+};
+
+export default data
